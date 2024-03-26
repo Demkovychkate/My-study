1760fb6e157b7dea403b1fe691c7cf56f0201181
Demkovychkate <143484048+Demkovychkate@users.noreply.github.com>
2024-03-26 16:04:05 +0200

    Add files via upload

---
+++ HW_33 Calculator with Node.js/README.md
@@ -0,0 +1,8 @@
+# 44.1
+
+My 44.1 homework
+Assignment:
+
+Create a module (using npm init or through JavaScript modules).
+Implement a module named "calculator".
+Each operation function (add, sub, div, mult) should be in a separate file.
---
+++ HW_33 Calculator with Node.js/index.js
@@ -0,0 +1,9 @@
+import add from './components/add.js';
+import sub from './components/sub.js';
+import mult from './components/mult.js';
+import div from './components/div.js';
+
+console.log('Addition:', add());
+console.log('Subtraction:', sub());
+console.log('Multiplication:', mult());
+console.log('Division:', div());
